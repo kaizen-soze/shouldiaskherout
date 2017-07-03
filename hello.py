fb075be4eca4e05f4582450d1c00a2a3c4ec6197
Kaizen Soze <kaizensoze42@gmail.com>
2017-07-03 19:59:27 -0400

    Non-working experiment with an optional routing paramater

--- hello.py
+++ hello.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<rawr>', defaults = {'rawr': None})
+def user(rawr):
+    print('Rawr is {0}'.format(rawr))
+    return render_template('name.html', name=rawr)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
